315711cb2cf6ab9cce050cf736c034ed64ac9071
Daniel Bremer Lützen <xabder@gmail.com>
2022-08-17 23:58:40 +0200

    AddRole and RemoveRole works, now working on roleembed

--- commands/addRole.js
+++ commands/addRole.js
@@ -10,8 +10,9 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.options.getUser('user');
     const role = interaction.options.getString('role');
+    const roleid = role.slice(3, role.length - 1);
     const member = interaction.guild.members.cache.get(user.id);
-    const roleObj = interaction.guild.roles.cache.find((r) => r.name === role);
+    const roleObj = interaction.guild.roles.cache.find((r) => r.id === roleid);
     if (!roleObj) {
       await interaction.reply({ content: `Could not find role ${role}`, ephemeral: true });
       return;
--- commands/removeRole.js
+++ commands/removeRole.js
@@ -10,8 +10,19 @@ module.exports = {
     .addStringOption((option) => option.setName('role').setDescription('The role to remove from the user').setRequired(true)),
   async execute(interaction) {
     const user = interaction.options.getUser('user');
+    const member = interaction.guild.members.cache.get(user.id);
     const role = interaction.options.getString('role');
-    await interaction.guild.members.cache.get(user.id).roles.remove(role);
-    await interaction.reply({ content: `${user.username} has been removed from the ${role} role!`, ephemeral: true });
+    const roleid = role.slice(3, role.length - 1);
+    const roleObj = interaction.guild.roles.cache.find((r) => r.id === roleid);
+    if (!roleObj) {
+      await interaction.reply({ content: `Could not find role ${role}`, ephemeral: true });
+      return;
+    }
+    if (member.roles.cache.has(roleObj.id)) {
+      await member.roles.remove(roleObj);
+      await interaction.reply({ content: `${user.username} has been removed from the ${role} role!`, ephemeral: true });
+    }
+    await interaction.reply({ content: `${user.username} doesn't have the role ${role}`, ephemeral: true });
+    return;
   },
 };
