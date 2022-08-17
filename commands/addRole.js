fb3ec7acb8e11b57d1d49685b7adae7ae627ff5d
Daniel Bremer Lützen <xabder@gmail.com>
2022-08-17 22:34:32 +0200

    Tried to fix taggin role, didnt work

--- commands/addRole.js
+++ commands/addRole.js
@@ -1,19 +1,26 @@
-// give user role
-
+// give user a role in the server (if they don't have it already) i should be able to tag the role
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('addrole')
-    .setDescription('Give a user a role')
-    .addUserOption((option) => option.setName('user').setDescription('The user to give a role to').setRequired(true))
+    .setDescription('Give a user a role in the server')
+    .addUserOption((option) => option.setName('user').setDescription('The user to give the role to').setRequired(true))
     .addStringOption((option) => option.setName('role').setDescription('The role to give the user').setRequired(true)),
   async execute(interaction) {
     const user = interaction.options.getUser('user');
-    console.log('User: ' + user);
-    const role = interaction.options.getRole('role');
-    console.log('Role: ' + role);
-    await interaction.guild.members.cache.get(user.id).roles.add(role);
-    await interaction.reply({ content: `${user.username} has been given the ${role} role!`, ephemeral: true });
+    const role = interaction.options.getString('role');
+    const member = interaction.guild.members.cache.get(user.id);
+    const roleObj = interaction.guild.roles.cache.find((r) => r.name === role);
+    if (!roleObj) {
+      await interaction.reply({ content: `Could not find role ${role}`, ephemeral: true });
+      return;
+    }
+    if (member.roles.cache.has(roleObj.id)) {
+      await interaction.reply({ content: `${user.username} already has the role ${role}`, ephemeral: true });
+      return;
+    }
+    await member.roles.add(roleObj);
+    await interaction.reply({ content: `${user.username} has been given the role ${role}`, ephemeral: true });
   },
 };
