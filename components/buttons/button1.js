99ceb049a6bc13521cfc8c5e673b179ab9efed4c
Daniel Bremer Lützen <xabder@gmail.com>
2022-09-21 03:47:46 +0200

    Test Create Channel

--- components/buttons/button1.js
+++ components/buttons/button1.js
@@ -5,7 +5,35 @@ module.exports = {
     name: 'ticket1',
   },
   async execute(interaction) {
-    // get name of user who clicked button and create a channel with their name, everyone is allowed to see the channel
+    // get name of user who clicked button and create a channel where the channel is named generel and then their name, the channel should be in the category named "Generel Support" inherit permissions from the category and then add the user who clicked the button to the channel and give them the permission to send messages, read messages and add reactions, mention the user who clicked the button and then send a message to the channel saying "Hello, how can I help you today?"
+    const username = interaction.user.username;
+    const channel = await interaction.guild.channels.create({name: `general-${username}`,
+      type: ChannelType.GUILD_TEXT,
+      parent: interaction.guild.channels.cache.find(
+        (channel) => channel.name === 'General Support'
+      ),
+      permissionOverwrites: [
+        {
+          id: interaction.guild.roles.everyone,
+          deny: PermissionsBitField.FLAGS.VIEW_CHANNEL,
+        },
+        {
+          id: interaction.user.id,
+          allow: PermissionsBitField.FLAGS.SEND_MESSAGES,
+          allow: PermissionsBitField.FLAGS.READ_MESSAGE_HISTORY,
+          allow: PermissionsBitField.FLAGS.ADD_REACTIONS,
+        },
+      ],
+    });
+    await interaction.reply({
+      content: `${interaction.user}`,
+      ephemeral: true,
+    });
+    await channel.send(`Hello, how can I help you today?`);
+  },
+};
+    /*
+    // get name of user who clicked button and create a channel where the , everyone is allowed to see the channel
     const channelName = interaction.user.username;
     const channel = await interaction.guild.channels.create({
       name: channelName,
@@ -27,3 +55,4 @@ module.exports = {
     await interaction.reply(`Your ticket has been created at ${channel}`);
   },
 };
+/*
