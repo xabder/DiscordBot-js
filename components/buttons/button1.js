86137867bab9b9d0c3c2bc8b85be70dd13f6dc1f
Daniel Bremer Lützen <xabder@gmail.com>
2022-09-21 03:49:01 +0200

    Edit button1

--- components/buttons/button1.js
+++ components/buttons/button1.js
@@ -7,11 +7,10 @@ module.exports = {
   async execute(interaction) {
     // get name of user who clicked button and create a channel where the channel is named generel and then their name, the channel should be in the category named "Generel Support" inherit permissions from the category and then add the user who clicked the button to the channel and give them the permission to send messages, read messages and add reactions, mention the user who clicked the button and then send a message to the channel saying "Hello, how can I help you today?"
     const username = interaction.user.username;
-    const channel = await interaction.guild.channels.create({name: `general-${username}`,
+    const channel = await interaction.guild.channels.create({
+      name: `general-${username}`,
       type: ChannelType.GUILD_TEXT,
-      parent: interaction.guild.channels.cache.find(
-        (channel) => channel.name === 'General Support'
-      ),
+      parent: interaction.guild.channels.cache.find((channel) => channel.name === 'General Support'),
       permissionOverwrites: [
         {
           id: interaction.guild.roles.everyone,
@@ -32,27 +31,3 @@ module.exports = {
     await channel.send(`Hello, how can I help you today?`);
   },
 };
-    /*
-    // get name of user who clicked button and create a channel where the , everyone is allowed to see the channel
-    const channelName = interaction.user.username;
-    const channel = await interaction.guild.channels.create({
-      name: channelName,
-      type: ChannelType.GuildText,
-      permissionOverwrites: [
-        {
-          id: interaction.guild.roles.everyone,
-          allow: [PermissionsBitField.Flags.ViewChannel],
-        },
-        {
-          id: interaction.user.id,
-          allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.ReadMessageHistory],
-        },
-      ],
-    });
-    // send a message to the channel
-    await channel.send(`Hello ${interaction.user.username}, welcome to your ticket!`);
-    // send a message to the user who clicked the button
-    await interaction.reply(`Your ticket has been created at ${channel}`);
-  },
-};
-/*
