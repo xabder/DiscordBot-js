c0e5d17f153b246a534721812746373bbf94758a
Daniel Bremer Lützen <xabder@gmail.com>
2022-09-22 03:44:16 +0200

    Tried setting emoji to evenlope

--- commands/ticket/setupTicket.js
+++ commands/ticket/setupTicket.js
@@ -23,10 +23,10 @@ module.exports = {
   },
   */
   async addButton(interaction) {
-    const button1 = new ButtonBuilder().setCustomId('button1').setLabel('Generel Support').setStyle(ButtonStyle.Danger).setEmoji('✉️');
-    const button2 = new ButtonBuilder().setCustomId('button2').setLabel('Admin Ansøgning').setStyle(ButtonStyle.Primary).setEmoji('1022317726670262283');
-    const button3 = new ButtonBuilder().setCustomId('button3').setLabel('Admin Klage').setStyle(ButtonStyle.Secondary).setEmoji('1022317877392572507');
-    const button4 = new ButtonBuilder().setCustomId('button4').setLabel('Køb/Handel').setStyle(ButtonStyle.Success).setEmoji('1022317929821388821');
+    const button1 = new ButtonBuilder().setCustomId('button1').setLabel('Generel Support').setStyle(ButtonStyle.Danger).setEmoji(':envelope:');
+    const button2 = new ButtonBuilder().setCustomId('button2').setLabel('Admin Ansøgning').setStyle(ButtonStyle.Primary);
+    const button3 = new ButtonBuilder().setCustomId('button3').setLabel('Admin Klage').setStyle(ButtonStyle.Secondary);
+    const button4 = new ButtonBuilder().setCustomId('button4').setLabel('Køb/Handel').setStyle(ButtonStyle.Success);
     const row = new ActionRowBuilder().addComponents(button1, button2, button3, button4);
     await interaction.reply({ content: 'Ticket Categories', components: [row] });
   },
