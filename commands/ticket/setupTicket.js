74ca2d7bbb21b3aa5f7ac9fab3e2dcfaad520d2d
Daniel Bremer Lützen <xabder@gmail.com>
2022-09-22 12:32:18 +0200

    figure out how to send messsage not reply

--- commands/ticket/setupTicket.js
+++ commands/ticket/setupTicket.js
@@ -1,33 +1,19 @@
 // It needs to make an channel and then ask for which ticket categories it needs to have, then send it in the newly created channel.
 
-const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('setupticket').setDescription('Setup a ticket system'),
 
   async execute(interaction) {
     this.addButton(interaction);
-    //this.modal(interaction);
   },
-  /*
-  async modal(interaction) {
-    const modal = new ModalBuilder().setCustomId('ticketsetup').setTitle('Ticket Setup');
-
-    const category = new TextInputBuilder().setCustomId('category').setLabel('Category').setStyle(TextInputStyle.Short);
-
-    const categories = new ActionRowBuilder().addComponents(category);
-
-    modal.addComponents(categories);
-
-    await interaction.showModal(modal);
-  },
-  */
   async addButton(interaction) {
     const button1 = new ButtonBuilder().setCustomId('ticket1').setLabel('Generel Support').setStyle(ButtonStyle.Danger).setEmoji('✉️');
     const button2 = new ButtonBuilder().setCustomId('ticket2').setLabel('Admin Ansøgning').setStyle(ButtonStyle.Primary).setEmoji('📮');
     const button3 = new ButtonBuilder().setCustomId('ticket3').setLabel('Admin Klage').setStyle(ButtonStyle.Secondary).setEmoji('📜');
     const button4 = new ButtonBuilder().setCustomId('ticket4').setLabel('Køb/Handel').setStyle(ButtonStyle.Success).setEmoji('💸');
     const row = new ActionRowBuilder().addComponents(button1, button2, button3, button4);
-    await interaction.reply({ content: 'Ticket Categories', components: [row] });
+    await interaction.channel.sendMessage({ content: 'Ticket Categories', components: [row] });
   },
 };
--- components/buttons/button1.js
+++ components/buttons/button1.js
@@ -8,9 +8,9 @@ module.exports = {
     // get name of user who clicked button and create a channel where the channel is named generel and then their name, the channel should be in the category named "Generel Support" inherit permissions from the category and then add the user who clicked the button to the channel and give them the permission to send messages, read messages and add reactions, mention the user who clicked the button and then send a message to the channel saying "Hello, how can I help you today?"
     const username = interaction.user.username;
     const channel = await interaction.guild.channels.create({
-      name: `general-${username}`,
+      name: `generel-${username}`,
       type: ChannelType.GUILD_TEXT,
-      parent: interaction.guild.channels.cache.find((channel) => channel.name === 'General Support'),
+      parent: interaction.guild.channels.cache.find((channel) => channel.name === 'Generel Support'),
     });
     channel.lockPermissions();
     channel.permissionOverwrites.create(interaction.user.id, { ViewChannel: true, SendMessages: true, AddReactions: true });
